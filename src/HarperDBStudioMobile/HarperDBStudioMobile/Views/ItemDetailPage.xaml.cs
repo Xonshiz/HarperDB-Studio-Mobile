74b44e3bdf919f9a7d1b62e768987b3c5ca1c904
Xonshiz <kanojia24.10@gmail.com>
2021-06-17 21:28:52 +0530

    Initial Code

---
+++ src/HarperDBStudioMobile/HarperDBStudioMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HarperDBStudioMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HarperDBStudioMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ src/HarperDBStudioMobile/HarperDBStudioMobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using HarperDBStudioMobile.Models;
+using HarperDBStudioMobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HarperDBStudioMobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
